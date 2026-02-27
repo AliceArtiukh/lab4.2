c0e8a82c22bf09ccec852a79ca10cebab5dc0eef
Людмила <lyudosshka_@knu.ua>
2026-02-27 20:33:24 +0200

    Added function to utils.py

--- utils.py
+++ utils.py
@@ -3,3 +3,11 @@ def factorial(n):
         return 1
     else:
         return n*factorial(n-1)
+def is_power_of_five(n):
+    if n <= 0:
+        return False
+    
+    while n % 5 == 0:
+        n = n // 5
+        
+    return n == 1
