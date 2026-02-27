f9c60a2f42ced9b46bfbb58ae3dc183f1d3e4e8f
milienakolot2025 <260503702+milienakolot2025@users.noreply.github.com>
2026-02-27 20:37:40 +0200

    Додано функцію fibonacci та виклик у main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from utils import factorial 
-
+from utils import fibonacci
 print(factorial(5)) 
+print("10-те число Фібоначчі:", fibonacci(10))
--- utils.py
+++ utils.py
@@ -3,3 +3,11 @@ def factorial(n):
         return 1
     else:
         return n*factorial(n-1)
+def fibonacci(n):
+    """Повертає n-те число Фібоначчі (0, 1, 1, 2, 3, 5...)"""
+    if n < 0:
+        return None
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
